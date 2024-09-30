b011f7e5d2fc82d49c51c6d80d3a60e37469c695
doing1024 <doing1024@outlook.it>
2024-09-30 09:18:14 +0800

    update

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-import pypandoc
+import sys
 import os
+import re
+import shutil
+import pypandoc
 import fire
 import toml
-import shutil
-import re
 
-class dlog(object):
+
+class Dlog:
+    "dlog主逻辑类"
+
     def __init__(self):
         pass
 
     def __readconfig(self):
         return toml.load("config.toml")
 
-    def __confirm(self,message,yes='y',no='n'):
+    def __confirm(self, message, yes='y', no='n'):
         want_to_do = input(f"{message} [{yes}/{no}]:")
-        while want_to_do != yes and want_to_do != no:
+        while want_to_do not in (yes, no):
             want_to_do = input(f"please answer {yes} or {no}:")
-        return True if want_to_do == yes else False
+        return bool(want_to_do == yes)
 
     def __newsite(self):
         if not self.__confirm(f"Do you want to create a new site at {os.getcwd()}?"):
             print("Abort!")
-            exit(1)
+            sys.exit(1)
         os.mkdir("posts")
         os.mkdir("themes")
-        with open("config.toml","w"):
+        with open("config.toml", "w"):
             pass
 
-    def build(self,i=False):
+    def build(self, i=False):
+        """实现dlog build命令"""
         config = self.__readconfig()
         if i:
             pypandoc.pandoc_download.download_pandoc()
@@ -35,21 +40,21 @@ class dlog(object):
         os.mkdir("build")
         theme = config["theme"]
         for file in os.listdir("posts"):
-            if os.path.isdir(f"posts/{file}"):
-                shutil.copytree(f"posts/{file}",f"build/{file}")
+            if os.path.isdir(file):
                 continue
-            print(file)
-            postbody = pypandoc.convert_file(f"posts/{file}","html")
-            with open(f"build/{file.split('.')[0]}.html","w") as output,open(f"themes/{theme}/template/post.html","r") as tmplt:
-                s = tmplt.read().replace("{{{postBody}}}",postbody).replace("file:///","/")
+            postbody = pypandoc.convert_file(f"posts/{file}", "html")
+            with open(f"build/{file.split('.')[0]}.html", "w") as output, open(f"themes/{theme}/template/post.html", "r") as tmplt:
+                s = tmplt.read().replace("{{{postBody}}}", postbody)
                 words = list(set(re.compile(r"\{\{\{.*\}\}\}").findall(s)))
                 for word in words:
-                    s = s.replace(word,config[word[3:-3]])
+                    s = s.replace(word, config[word[3:-3]])
                 output.write(s)
 
-    def new(self,type):
-        if type == "site":
+    def new(self, typ):
+        """实现dlog new"""
+        if typ == "site":
             self.__newsite()
 
+
 if __name__ == "__main__":
-    fire.Fire(dlog)
+    fire.Fire(Dlog)
--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 cd /run/media/doing/Files/Note/
-pypy3 main.py build
+python3 main.py build
 git add .
 git commit -m "update"
 git push origin main
